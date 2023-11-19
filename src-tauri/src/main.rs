7a838b177130b3c29c1181e48bd4e649ebaa6e9d
vanha777 <vanha777@icloud.com>
2023-11-20 08:43:23 +1100

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,8 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+pub fn main() {
+  // Change demo_mobile_app to the name of your app! 
+  app::AppBuilder::new().run();
+}
